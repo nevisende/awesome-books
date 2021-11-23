3ed5b8e14e679d1c4e59941b77e5125d84342a34
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-23 14:20:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-import { helperObj } from './data.js';
+import Helper from './data.js';
+
+const helperObj = new Helper();
 
 // display the books in the localStorage on load
 helperObj.display();
 
 // Event listener for the addition to localStorage and rendering of HTML to page
-document.querySelector('#add-btn').addEventListener('click', (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  if (title === '' || author === '') {
-    return;
-  }
-  helperObj.add(title, author);
-  document.querySelectorAll('.remove').forEach((elem) => {
-    elem.addEventListener('click', (e) => helperObj.remove(e, e.currentTarget.dataset.id));
-  });
+document.querySelector('#add-btn').addEventListener('click', () => {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    if (title === '' || author === '') return;
+    helperObj.add(title, author);
+    document.querySelectorAll('.remove').forEach((elem) => {
+        elem.addEventListener('click', (e) => helperObj.remove(e, e.currentTarget.dataset.id));
+    });
 });
