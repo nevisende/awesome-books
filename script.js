8150e51047c8add4997287c517d6d7facc318845
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-23 14:24:15 +0200

    fix linters issues

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ helperObj.display();
 
 // Event listener for the addition to localStorage and rendering of HTML to page
 document.querySelector('#add-btn').addEventListener('click', () => {
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    if (title === '' || author === '') return;
-    helperObj.add(title, author);
-    document.querySelectorAll('.remove').forEach((elem) => {
-        elem.addEventListener('click', (e) => helperObj.remove(e, e.currentTarget.dataset.id));
-    });
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  if (title === '' || author === '') return;
+  helperObj.add(title, author);
+  document.querySelectorAll('.remove').forEach((elem) => {
+    elem.addEventListener('click', (e) => helperObj.remove(e, e.currentTarget.dataset.id));
+  });
 });
