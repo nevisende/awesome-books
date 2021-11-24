e05f1d420b8c182ba890df5e43162ea4ee2150d4
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-24 09:21:41 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,25 +2,23 @@
 /* eslint-disable class-methods-use-this */
 
 export default class Utils {
-  displayHTML(title, author, id) {
-    return `
-      <li class="book" style="list-style:none;">
-        <p>${title}</p>
-        <p>${author}</p>
-        <button class="remove" data-id=${id}>Remove</button>
-        <hr>
-      </li>
+    displayHTML(title, author, id) {
+        return `
+    <li class="book" style="list-style:none;">
+    <p><span class="title">"${title}"</span> by <span class="author">${author}</span></p>
+    <button class="remove" data-id=${id}>Remove</button>
+  </li>
     `;
-  }
+    }
 
-  render(title, author, id) {
-    document
-      .querySelector('#lists')
-      .insertAdjacentHTML('beforeend', this.displayHTML(title, author, id));
-  }
+    render(title, author, id) {
+        document
+            .querySelector('#lists')
+            .insertAdjacentHTML('beforeend', this.displayHTML(title, author, id));
+    }
 
-  clearInput() {
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
-  }
+    clearInput() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+    }
 }
