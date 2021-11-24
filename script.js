4a734156209ee624d65172283944311674a5f324
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-24 09:53:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const bookObj = new Book();
 bookObj.display();
 
 document.querySelector('#add-btn').addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    if (title === '' || author === '') {
-        document.querySelector('.error').innerText = 'Kindly make sure title and author are filled';
-        return;
-    }
-    document.querySelector('.error').innerText = '';
-    bookObj.add(title, author);
-    document.querySelectorAll('.remove').forEach((elem) => {
-        elem.addEventListener('click', (e) => bookObj.remove(e, e.currentTarget.dataset.id));
-    });
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  if (title === '' || author === '') {
+    document.querySelector('.error').innerText = 'Kindly make sure title and author are filled';
+    return;
+  }
+  document.querySelector('.error').innerText = '';
+  bookObj.add(title, author);
+  document.querySelectorAll('.remove').forEach((elem) => {
+    elem.addEventListener('click', (e) => bookObj.remove(e, e.currentTarget.dataset.id));
+  });
 });
