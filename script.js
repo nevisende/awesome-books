43b73ca806b813ae1f000e85dd27deb80dffc9e4
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-11-25 12:03:35 +0300

    Fix the problems

--- script.js
+++ script.js
@@ -6,47 +6,51 @@ const bookObj = new Book();
 bookObj.display();
 
 document.querySelector('#add-btn').addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    if (title === '' || author === '') {
-        document.querySelector('.error').innerText = 'Kindly make sure title and author are filled';
-        return;
-    }
-    document.querySelector('.error').innerText = '';
-    bookObj.add(title, author);
-    document.querySelectorAll('.remove').forEach((elem) => {
-        elem.addEventListener('click', (e) => bookObj.remove(e, e.currentTarget.dataset.id));
-    });
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  if (title === '' || author === '') {
+    document.querySelector('.error').innerText = 'Kindly make sure title and author are filled';
+    return;
+  }
+  document.querySelector('.error').innerText = '';
+  bookObj.add(title, author);
+  document.querySelectorAll('.remove').forEach((elem) => {
+    elem.addEventListener('click', (e) => bookObj.remove(e, e.currentTarget.dataset.id));
+  });
 });
 
 function dateHandler() {
-    document.querySelector('#time').innerText = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL_WITH_SECONDS);
-    setTimeout(dateHandler, 1000);
+  // eslint-disable-next-line no-undef
+  document.querySelector('#time').innerText = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL_WITH_SECONDS);
+  setTimeout(dateHandler, 1000);
 }
 dateHandler();
 
 const listLink = document.getElementById('list-link');
 const addNewLink = document.getElementById('add-new-link');
-const contactLink = document.getElementById('contact-link')
+const contactLink = document.getElementById('contact-link');
+const listBooks = document.getElementById('list-books');
+const addNew = document.getElementById('add-new');
+const contact = document.getElementById('contact');
 
 listLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    listLink.style.display = 'block';
-    addNewLink.style.display = 'none';
-    contactLink.style.display = 'none';
+  e.preventDefault();
+  listBooks.style.display = 'block';
+  addNew.style.display = 'none';
+  contact.style.display = 'none';
 });
 
 addNewLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    listLink.style.display = 'none';
-    addNewLink.style.display = 'block';
-    contactLink.style.display = 'none';
+  e.preventDefault();
+  listBooks.style.display = 'none';
+  addNew.style.display = 'block';
+  contact.style.display = 'none';
 });
 
 contactLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    listLink.style.display = 'none';
-    addNewLink.style.display = 'none';
-    contactLink.style.display = 'block';
+  e.preventDefault();
+  listBooks.style.display = 'none';
+  addNew.style.display = 'none';
+  contact.style.display = 'block';
 });
